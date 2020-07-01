08fc86b496e2c2c5996e26b850018990bd7e02b5
SWITED <n.yakovlev@g.nsu.ru>
2020-07-02 00:21:15 +0700

    3rdlab

---
+++ Tetris/src/main/java/Application/Player.java
@@ -0,0 +1,9 @@
+package Application;
+
+import javafx.util.Pair;
+
+public class Player extends Pair<Integer, String> {
+    public Player(Integer key, String value) {
+        super(key, value);
+    }
+}
